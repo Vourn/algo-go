f8e0a179e880ca9b536512962967509b8b8b9745
Vourn <3290759979@qq.com>
2022-01-05 17:30:27 +0800

    binary_tree traversal

--- binarytree/traversal.go
+++ binarytree/traversal.go
@@ -11,6 +11,33 @@ func PreorderTraversal(root *TreeNode) {
     PreorderTraversal(root.Right)
 }
 
+// 前序非递归
+func PreorderTra(root *TreeNode) []int {
+    if root == nil {
+	return nil
+    }
+    result := make([]int, 0)
+    stack := make([]*TreeNode, 0) // 保存已访问结点，用于原路返回
+
+    for root != nil || len(stack) != 0 {
+	for root != nil {
+	    // 先保存结果再指向左结点
+	    result = append(result, root.Val)
+	    stack = append(stack, root)
+	    root = root.Left
+	}
+        // 遍历完左结点，遍历右结点
+	node := stack[len(stack) - 1]
+        stack = stack[: len(stack) - 1]
+	root = root.Right
+    }
+    return result
+}
+
+// 中序非递归
+
+// 后序非递归
+
 type TreeNode struct {
     Val int
     Left *TreeNode
