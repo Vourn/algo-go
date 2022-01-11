df4c55b87df0b57237a5cde58f273bc46e439522
Vourn <3290759979@qq.com>
2022-01-11 09:41:13 +0800

    binary tree exer

--- binarytree/exercise.go
+++ binarytree/exercise.go
@@ -38,3 +38,43 @@ func maxDepth(root *TreeNode) int {
     }
     return right + 1
 }
+
+// 二叉树中的最大路径和
+type ResultType struct {
+    SinglePath int
+    MaxPath int
+}
+func maxPathSum(root *TreeNode) int {
+    result := helper(root)
+    return result.MaxPath
+}
+func helper(root *TreeNode) ResultType {
+    if root == nil {
+	return ResultType {
+	    SinglePath: 0
+	    MaxPath: -(1 << 31)
+	}
+    }
+    // divide
+    left := helper(root.Left)
+    right := helper(root.Right)
+
+    // conquer
+    result := ResultType{}
+    // 单边最大值
+    if left.SinglePath > right.SinglePath {
+	result.SinglePath = max(left.SinglePath + root.Val, 0)
+    } else {
+	result.SinglePath = max(right.SinglePath + root.Val, 0)
+    }
+    // 路径最大值
+    maxPath := max(left.MaxPath, right.MaxPath)
+    result.MaxPath = max(maxPath, left.SinglePath + right.SinglePath + root.Val)
+    return result
+}
+func max(a, b int) int {
+    if a > b {
+	return a
+    }
+    return b
+}
