f45d8e271982bdebeebd66b91f96908c299ddf3d
Vourn <3290759979@qq.com>
2022-01-18 10:58:19 +0800

    link_list exer

--- linklist/link_list.go
+++ linklist/link_list.go
@@ -38,3 +38,35 @@ func deleDuplicates(head *ListNode) *ListNode {
     }
     return dummy.Next
 }
+
+// 反转链表
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode
+    for head != nil {
+	temp := head.Next
+        head.Next = prev
+        prev = head
+	head = temp
+    }
+    return prev
+}
+
+// 反转从位置left到right的链表。请使用一趟扫描完成反转
+func reverseBetween(head *ListNode, left, right int) *ListNode {
+    dummy := &ListNode{Val: 0}
+    dummy.Next = head
+    prev := dummy
+    // 遍历到left位置
+    for i := 0; i < left -1; i++ {
+	prev = prev.Next
+    } 
+    cur := prev.Next
+    // left到right区间节点反转
+    for i := 0; i < right - left; i++ {
+	next := cur.Next
+        cur.Next = next.Next
+        next.Next = prev.Next
+	prev.Next = next
+    }
+    return dummy.Next
+}
